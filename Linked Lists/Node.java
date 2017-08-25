2f4457fcabcd2dd05f58c0be49767be6243e342f
Aniketh Sukhtankar <iamaniketh@gmail.com>
2017-08-24 20:22:40 -0400

    Create Node.java

---
+++ Linked Lists/Node.java
@@ -0,0 +1,35 @@
+package linkedlists;
+
+public class Node {
+    int data;
+    Node next = null;
+
+    public Node(int d){
+        data = d;
+    }
+
+    void appendToTail(int d){
+        Node end = new Node(d);
+        Node n = this;
+        while(n.next != null){
+            n = n.next;
+        }
+        n.next = end;
+    }
+
+    Node deleteNode(Node head, int d){
+        Node n = head;
+        if(n.data == d){
+            return head.next;
+        }
+
+        while(n.next != null){
+            if(n.next.data == d){
+                n.next = n.next.next;
+                return head;
+            }
+            n=n.next;
+        }
+        return head;
+    }
+}
