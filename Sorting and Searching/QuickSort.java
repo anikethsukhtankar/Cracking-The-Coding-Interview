018d31449e8920a8bdd2e42b012fc3cb836562ed
Aniketh Sukhtankar <iamaniketh@gmail.com>
2018-01-18 16:39:45 -0500

    Implemented 3 Way Partitioning Quicksort
    Provides almost linear performance for files with a lot of duplicates

--- Sorting and Searching/QuickSort.java
+++ Sorting and Searching/QuickSort.java
@@ -9,16 +9,15 @@ public class QuickSort {
         }
     }
 
-    private static int partition(int[] arr, int low, int high) {
-
+    private static int partition(int[] arr, int low, int high){
         int pivot = arr[high];
 
         int i = low-1;
 
         for(int j=low; j<high; j++){
-            if(arr[j] <= pivot) {
+            if(arr[j] <= pivot){
                 i++;
-                swap(arr, i, j);
+                swap(arr,i,j);
             }
         }
 
@@ -32,9 +31,23 @@ public class QuickSort {
         arr[j] = temp;
     }
 
+    private static void threeWayQuickSort(int[] arr, int lo, int hi){
+        int lt = lo;
+        int gt = hi;
+        int v = arr[lo];
+        int i = lo;
+        while(i <= gt) {
+            if (arr[i] < v) swap(arr, lt++, i++);
+            else if (arr[i] > v) swap(arr, i, gt--);
+            else i++;
+        }
+        sort(arr,lo,lt-1);
+        sort(arr,gt+1,hi);
+    }
+
     public static void main(String[] args) {
         int[] input = {8,6,4,1,2,9,0,3,3};
-        sort(input,0,input.length-1);
+        threeWayQuickSort(input,0,input.length-1);
         for(int x : input)
             System.out.print(x);
     }
